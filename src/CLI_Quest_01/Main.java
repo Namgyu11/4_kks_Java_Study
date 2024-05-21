74b0269597744380ba8d0dc1b80b85ace2a4bd64
Namgyu11 <hagaji3@naver.com>
2024-05-21 13:15:24 +0900

    refactor: Main 클래스 책임 분리화 진행

--- src/CLI_Quest_01/Main.java
+++ src/CLI_Quest_01/Main.java
@@ -1,6 +1,6 @@
 package CLI_Quest_01;
 
-import CLI_Quest_01.menu.MenuType;
+import CLI_Quest_01.order.OrderManager;
 import CLI_Quest_01.util.MenuUtils;
 
 import java.util.Scanner;
@@ -9,42 +9,8 @@ public class Main {
 
       public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
-            Order order = new Order();
             MenuUtils menuUtils = MenuUtils.getInstance();
-
-            System.out.println("=======================================================\n");
-            System.out.println("환영합니다. 햄버거 가게입니다.");
-            System.out.println("주문을 도와드리겠습니다.");
-            boolean exit = false;
-            while (!exit) {
-                  System.out.println("\n=======================================================\n");
-                  System.out.println("*** 메인 메뉴\n메뉴를 선택해주세요.\n");
-                  System.out.println("0. 주문 나가기\n1. 햄버거\n2. 스페셜 햄버거\n");
-                  System.out.print("메뉴 번호 입력 : ");
-                  int choice = scanner.nextInt();
-                  System.out.println();
-                  switch (choice) {
-                        case 0:
-                              System.out.println("이용해주셔서 감사합니다. 안녕히 가세요.");
-                              return;
-                        case 1:
-                              if (menuUtils.addMainMenu(order, MenuType.BURGER, scanner)) {
-                                    exit = true;
-                              }
-                              break;
-                        case 2:
-                              if (menuUtils.addMainMenu(order, MenuType.SPECIAL_BURGER, scanner)) {
-                                    exit = true;
-                              }
-                              break;
-                        default:
-                              System.out.println("잘못된 입력입니다.");
-                  }
-            }
-            int totalPrice = order.displayOrder(menuUtils.selectQuantity(scanner));
-            menuUtils.calculation(totalPrice, scanner);
-
-            System.out.println("주문이 완료되었습니다. 이용해주셔서 감사합니다.");
-            System.out.println("================  주문 종료  ==================\n");
+            OrderManager orderManager = new OrderManager(scanner, menuUtils);
+            orderManager.startOrdering();
       }
 }
