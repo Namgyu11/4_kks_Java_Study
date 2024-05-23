a631c27b75510ed0bd7033917351c1c22080ff36
Namgyu11 <hagaji3@naver.com>
2024-05-24 00:32:41 +0900

    feat : 주문 처리 중 올바른 음악 트랙 처리를 위해 BackgroundMusic 클래스와 동기화.

--- src/CLI_Quest_01/order/OrderProcessor.java
+++ src/CLI_Quest_01/order/OrderProcessor.java
@@ -1,9 +1,9 @@
 package CLI_Quest_01.order;
 
+import CLI_Quest_01.music.BackgroundMusic;
 import CLI_Quest_01.util.InputUtils;
-import CLI_Quest_01.type.OrderConstants;
-import CLI_Quest_01.type.MenuType;
 import CLI_Quest_01.util.MenuUtils;
+import CLI_Quest_01.type.MenuType;
 
 import java.util.Scanner;
 
@@ -12,12 +12,15 @@ public class OrderProcessor {
       private final OrderList orderList;
       private final MenuUtils menuUtils;
       private final Scanner scanner;
+      private final BackgroundMusic backgroundMusic;
 
-      public OrderProcessor(String name, OrderList orderList, MenuUtils menuUtils, Scanner scanner) {
+      public OrderProcessor(String name, OrderList orderList,
+              MenuUtils menuUtils, Scanner scanner, BackgroundMusic backgroundMusic) {
             this.name = name;
             this.orderList = orderList;
             this.menuUtils = menuUtils;
             this.scanner = scanner;
+            this.backgroundMusic = backgroundMusic;
       }
 
       public void processOrder() {
@@ -30,7 +33,8 @@ public class OrderProcessor {
                   System.out.println("*** 메인 메뉴\n메뉴를 선택해주세요.\n");
                   System.out.println("0. 주문 나가기\n1. 햄버거\n2. 스페셜 햄버거\n");
 
-                  int choice = InputUtils.getValidIntInput(scanner, "메뉴 번호 입력 : ", OrderConstants.MIN_MENU_CHOICE.getValue(), OrderConstants.MAX_BURGER_CHOICE.getValue());
+                  int choice = InputUtils.getValidIntInput(scanner, "메뉴 번호 입력 : ",
+                          0, 2);
 
                   switch (choice) {
                         case 0 -> exit = true;
@@ -40,7 +44,8 @@ public class OrderProcessor {
                   }
 
                   if (!exit) {
-                        String continueOrder = InputUtils.getYesOrNoInput(scanner, "주문을 계속하시겠습니까? (Y/N): ");
+                        String continueOrder = InputUtils.getYesOrNoInput(scanner,
+                                "주문을 계속하시겠습니까? (Y/N): ");
                         if (continueOrder.equals("n")) {
                               exit = true;
                         }
@@ -53,6 +58,8 @@ public class OrderProcessor {
                   System.out.println(name + "님의 주문이 완료되었습니다.");
             }
             System.out.println(name + "님이 주문을 종료합니다.\n");
+
+            backgroundMusic.nextTrack();
       }
 
       public String getName() {
